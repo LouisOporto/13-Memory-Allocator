d5e8bfcb4dba0734125ffd13d9fc08953f272a2e
LouisOporto <louisoporto042@csu.fullerton.edu>
2024-11-12 17:08:21 -0800

    Added realloc to interface

--- memory_allocator.c
+++ memory_allocator.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> // Future version will use heap-based memory allocation
+// No reliance on stdlib.h
 
 #include "memory_allocator.h"
 
-
+// Define memory interface
 void* modMalloc(int size) {
     printf("Implemented malloc\n");
     return malloc(size);
+}
+
+void* modRealloc(void* ptr, int size) {
+    printf("Implemented realloc\n");
+    return realloc(ptr, size);
 }
 
 void modFree(void* ptr) {
--- memory_allocator.h
+++ memory_allocator.h
@@ -1,11 +1,18 @@
 #ifndef MEMORY_ALLOCATOR_H
 #define MEMORY_ALLOCATOR_H
 
+// TODO names will be rewritten to just malloc, realloc, free (no mod prefix)
+
 // Define memory interface
-void* modMalloc(int size);
+void* modMalloc(int size); // malloc reset allocated blocks to 0
+
+void* modRealloc(void* ptr, int size);
 
 void modFree(void* ptr);
 
 // Future editions include
 // Utilizing heap-based memory allocation
+
+// All strucures are store in this block
+// Minheap for freeb blocs in final version, but arrays for now
 #endif
