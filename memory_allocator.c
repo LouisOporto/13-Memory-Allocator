5537cb22cd767e938950bc1919103b3ae8eec212
LouisOporto <louisoporto042@csu.fullerton.edu>
2024-11-18 11:59:23 -0800

    logging info added

--- memory_allocator.c
+++ memory_allocator.c
@@ -11,20 +11,22 @@ void* modMalloc(int size) {
         fprintf(stderr, __FILE__ ":%d malloc failed\n", __LINE__);
         exit(1);
     }
-    fprintf(stderr, __FILE__ ":%d: malloc(%d) = %p\n", __LINE__, size, ptr);
-    printf("Implemented malloc\n");
-    return malloc(size);
+    fprintf(stderr, __FILE__ ":%d: malloc(%lu) = %p\n", __LINE__, size, ptr);
+    return ptr;
 }
 
 void* modRealloc(void* ptr, int size) {
     void *newPtr = realloc(ptr, size); // Implement this
-    printf("Implemented realloc\n");
+    if (newPtr == NULL) {
+        fprintf(stderr, __FILE__ ":%d realloc failed\n", __LINE__);
+        exit(1);
+    }
+    fprintf(stderr, __FILE__ ":%d: realloc(%p, %lu) = %p\n", __LINE__, ptr, size, newPtr);
     return newPtr;
 }
 
 void modFree(void* ptr) {
+    fprintf(stderr, __FILE__ ": free(%p)\n", ptr);
     free(ptr); // Implement this
-    fprintf(stderr, __FILE__ ":%d: free(%p)\n", __LINE__, ptr);
-    printf("Implemented free\n");
 }
 
