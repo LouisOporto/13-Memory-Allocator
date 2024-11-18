c61a50a3a85465f2212e814b1438bcba855ca30f
LouisOporto <louisoporto042@csu.fullerton.edu>
2024-11-18 12:34:27 -0800

    Added memory block array

---
+++ malloc.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "malloc.h"
+
+// Define memory interface
+void* modMalloc(int size) {
+    void *ptr = malloc(size); // Implement this
+    if (ptr == NULL) {
+        fprintf(stderr, __FILE__ ":%d malloc failed\n", __LINE__);
+        exit(1);
+    }
+    fprintf(stderr, __FILE__ ":%d: malloc(%lu) = %p\n", __LINE__, size, ptr);
+    return ptr;
+}
+
+void* modRealloc(void* ptr, int size) {
+    void *newPtr = realloc(ptr, size); // Implement this
+    if (newPtr == NULL) {
+        fprintf(stderr, __FILE__ ":%d realloc failed\n", __LINE__);
+        exit(1);
+    }
+    fprintf(stderr, __FILE__ ":%d: realloc(%p, %lu) = %p\n", __LINE__, ptr, size, newPtr);
+    return newPtr;
+}
+
+void modFree(void* ptr) {
+    fprintf(stderr, __FILE__ ": free(%p)\n", ptr);
+    free(ptr); // Implement this
+}
+
+// Define memory block structure
+MemoryBlock* memoryBlock;
